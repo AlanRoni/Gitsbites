e96740177c6b1bb17c290ed1c2ed94d9d0d6b86e
Alan Roni <alanronimathew@gmail.com>
2024-11-12 08:27:29 +0530

    Cart draft done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'profile_page.dart';
+import 'cart_page.dart'; // Import the CartPage
 
 void main() {
   runApp(MyApp());
@@ -26,15 +27,32 @@ class HomePage extends StatelessWidget {
         title: Text('Home Page'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate to ProfilePage when tapped
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ProfilePage()),
-            );
-          },
-          child: Text('Go to Profile'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Button to navigate to ProfilePage
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ProfilePage()),
+                );
+              },
+              child: Text('Go to Profile'),
+            ),
+            SizedBox(height: 20), // Space between buttons
+
+            // Button to navigate to CartPage
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CartPage()),
+                );
+              },
+              child: Text('Go to Cart'),
+            ),
+          ],
         ),
       ),
     );
