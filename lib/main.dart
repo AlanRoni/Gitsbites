b93e1cde0c30516171e21c8ec3ae22edb95bf534
ALON JOHN <alonjohn76@gmail.com>
2025-01-20 21:54:56 +0530

    debug removed

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Kioski App',
+      debugShowCheckedModeBanner: false, // Removes the debug banner
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
